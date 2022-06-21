e8f4179989c9bb50532afb861b23e40ffbe504d7
CKrawczyk <coleman.krawczyk@gmail.com>
2022-06-21 15:03:16 +0100

    Second pass over Sphinx notes
    Minor cleanup of the notes.  Included the latest versoin of `index.rst` in the notes.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,7 +11,6 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import os
-import sphinx_rtd_theme    # noqa: F401
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
 
