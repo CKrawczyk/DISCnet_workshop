3964cc8f9a0106b36df3061f11fc03b555bf7ece
ntbfin00 <nathan.findlay@port.ac.uk>
2023-03-06 15:02:59 +0000

    Added function for extracting image specific DataFrames.

--- reduction.py
+++ reduction.py
@@ -1,4 +1,14 @@
 import pandas as pd
 import numpy as np
 
+# function to separate the DataFrame into DataFrames for each image
+def separate_img(df):
+    subject_id = df['subject_id']
+    ids = subject_id.unique()
+    ids = sorted(ids)
 
+    img_df = []
+    for i,ID in enumerate(ids):
+        img_df.append(df.loc[subject_id == ID])
+
+    return img_df
