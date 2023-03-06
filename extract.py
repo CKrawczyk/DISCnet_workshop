e54b859b8446d1d46c623f132a04bb0dc65b8bda
Jamie <jamie.donald-mccann@port.ac.uk>
2023-03-06 14:46:00 +0000

    Change how functions are applied. No longer rely on .apply() Renamed output from annotations to Answer.

--- extract.py
+++ extract.py
@@ -2,11 +2,13 @@ import pandas as pd
 import json
 import argparse
 
-def T0(row):
+def T0(shared_df, annotations_df):
     '''
     Extract the required variables for task 0.
     '''
-    return row[0]['value']
+    new_df = pd.concat([shared_df, annotations_df.apply(lambda x: x[0]['value'])],
+                       axis=1)
+    return new_df.rename(columns={'annotations':"Answer"})
 
 # Parse file name for script.
 parser = argparse.ArgumentParser()
@@ -19,6 +21,7 @@ df = pd.read_csv(args.fname)
 # Convert string of list of dicts.
 # Is a pandas Series.
 annot_df = df['annotations'].apply(json.loads)
+shared_df = df[['classification_id', 'user_id', 'subject_ids']]
 
 # Define dictionary of functions to apply.
 func_dict = {"T0": T0}
@@ -26,8 +29,6 @@ func_dict = {"T0": T0}
 # Loop over functions
 for fi in func_dict.keys():
     # Define new array.
-    new_df = pd.concat([df[['classification_id', 'user_id', 'subject_ids']],
-                        annot_df.apply(func_dict[fi])],
-                       axis=1)
+    new_df = func_dict[fi](shared_df, annot_df)
     new_df.to_csv(f"{fi}.csv")
 
