efcd1aba95b2cedbce0850f1b701f4193293258b
Tian Li <52313102+astroskylee@users.noreply.github.com>
2023-03-06 16:03:01 +0000

    Task3 function inside extract.py file

--- extract.py
+++ extract.py
@@ -20,6 +20,25 @@ def T0(shared_df, annotations_df):
     return new_df.rename(columns={'annotations': "Answer"})
 
 
+def discnet_t3(shared_df):
+    # Create empty array
+    df_t3 = np.zeros((len(shared_df),3))
+    # Add r, x, y to array, ignore empty lines
+    for i in range(len(shared_df)):
+        try:
+            df_t3[i,0] = shared_df.iloc[i][3]['value'][0]['r']
+            df_t3[i,1] = shared_df.iloc[i][3]['value'][0]['x']
+            df_t3[i,2] = shared_df.iloc[i][3]['value'][0]['y']
+        except:
+            pass
+    # Mark empty lines as Nan
+    df_t3[df_t3 == 0] = np.nan
+    # Transfer into Pd dataframe
+    d_pre = {'r': df_t3[:,0], 'x': df_t3[:,1],'y': df_t3[:,2]}
+    df_t3_pre = pd.DataFrame(data=d_pre)
+    return df_t3_pre
+
+
 # Parse file name for script.
 parser = argparse.ArgumentParser()
 parser.add_argument('--fname', help='Path to file.', required=True)
