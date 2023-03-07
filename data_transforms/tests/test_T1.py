f5d8c6c4fe2b8b12e126cc4b8c0ee52967f1bb7c
Luke Weisenbach <luke.weisenbach@port.ac.uk>
2023-03-07 15:48:58 +0000

    flake8

--- data_transforms/tests/test_T1.py
+++ data_transforms/tests/test_T1.py
@@ -6,8 +6,13 @@ import numpy as np
 
 class TestExtractT1(unittest.TestCase):
     def test_extract_t1(self):
-        df_in_shared = pd.DataFrame([["id1_1", "id1_2", "id1_3"], ["id2_1", "id2_2", "id2_3"]], columns = ["id1", "id2", "id3"])
-        df_in_annotated = pd.DataFrame([[[{}, {"value":2}]], [[{}, {"value":"hungry"}]]], columns = "annotations")
+        df_in_shared = pd.DataFrame(
+            [
+                ["id1_1", "id1_2", "id1_3"], 
+                ["id2_1", "id2_2", "id2_3"]
+            ], 
+            columns = ["id1", "id2", "id3"])
+        df_in_annotated = pd.DataFrame([[[{}, {"value": 2}]], [[{}, {"value": "hungry"}]]], columns = "annotations")
 
         df_expected = pd.concat(
             [
