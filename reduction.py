2d77cf876f1da3256eeba3c0f5117d3043b52a02
Wolfgang <wolfgang@wolfgangs-mbp.d.port.ac.uk>
2023-03-07 12:17:32 +0000

    Commit that updates the documentation and task2 function. Minor updates on the structure to pass dataframes rather than image IDs since this will work better for the input information we obtain from the extraction group.

--- reduction.py
+++ reduction.py
@@ -1,3 +1,5 @@
+''' A python script that reduces that Zooniverse data from the extraction script. '''
+
 import pandas as pd
 import numpy as np
 import sklearn
@@ -10,7 +12,28 @@ import sklearn
 
 
 # evaluate_task
-def evaluate_tasks(imgid, specfic_task_evaluation, task_threshold):
+def evaluate_tasks(img_df, specfic_task_evaluation, task_threshold):
+    ''' A function frame for task evaluation, which checks if the threshold of task evaluations is passed. It further defines what the output of task evaluations must be.
+
+    Parameters
+    __________
+    img_df : Dataframe
+             Image Dataframe
+    specific_task_evaluation: Python Function
+             Function that evaluates specific
+    task_threshold : int
+             The number of minimum people that are required for this specific task to evaluate consensus 
+
+    Returns
+    _______
+    consensus : Task specific
+                Different tasks will return different consensus summary statistics
+    consensus_flag : bool
+                Flag that returns true or false depending on wether a consensus is reached or not
+
+    aux_info : Task specific
+                Additional information relating to the task
+    '''
 
     consensus = None  # contains the information describing what the consensus is
     censensus_reached = False  # contains wether or not a consensus has been reached
@@ -18,7 +41,7 @@ def evaluate_tasks(imgid, specfic_task_evaluation, task_threshold):
 
     if count_user_evaluations(imgid) > task_threshold:
 
-    consensus, consensus_reached, aux_info = specific_task_evaluation(imgid)
+          consensus, consensus_reached, aux_info = specific_task_evaluation(img_df)
 
     return consensus, consensus_reached, aux_info
 
@@ -26,13 +49,13 @@ def evaluate_tasks(imgid, specfic_task_evaluation, task_threshold):
 
 
 # task 0
-def task0(imgid):
+def task0(img_df):
     #dostuff
     return consensus, consensus_reached, aux_info
 
 
 # task 1
-def task1(imgid):)
+def task1(img_df):)
     #dostuff
     return consensus, consensus_reached, aux_info
 
@@ -40,9 +63,30 @@ def task1(imgid):)
 
 # compute_mean_and_std
 
+# get_coordinates
 
 # task 2    
-def task2(imgid, coordinates, eps, min_samples):
+def task2(img_df):
+    ''' A function that evaluates task2 assumming that the minimum threshold of evaluations is met.
+
+    Parameters
+    __________
+    img_df : Dataframe
+             Image Dataframe
+    Returns
+    _______
+    consensus : Task specific
+                Different tasks will return different consensus summary statistics
+    consensus_flag : bool
+                Flag that returns true or false depending on wether a consensus is reached or not
+  
+    aux_info : Task specific
+                Additional information relating to the task
+    '''
+
+    coordinates = get_coordinates(img_df)
+    eps = 0.5
+    min_samples = 5
 
     # read for imgid the x,y coordinates out
     # give them to the clustering function
@@ -55,18 +99,22 @@ def task2(imgid, coordinates, eps, min_samples):
     for i in range(clustering.n_features_in_):
         
         center_list[i] = np.mean(coordinates[clustering.labels_==i], axis = 0)
-        
-    return center_list
+    
+    consensus_flag = 1
+    if clustering.n_features_in_ == 0:
+  	consensus_flag = 0
+
+    return center_list, consensus_flag, 
 
  
 # task 3
-def task3(imgid):
+def task3(img_df):
     #dostuff
     return consensus, consensus_reached, aux_info
 
 
 # task 4
-def task4(imgid):
+def task4(img_df):
     #dostuff
     return consensus, consensus_reached, aux_info
 
