f6f7afd34226b76327a8a87bde862e81837f18bd
cly20 <cly20@sussex.ac.uk>
2023-03-07 15:50:12 +0000

    Unit test for Task 4 function

---
+++ data_transforms/tests/test_T4.py
@@ -0,0 +1,30 @@
+import unittest
+from data_transforms.extract import T4
+
+
+class TestT4(unittest.TestCase):
+    def test_first(self):
+        '''Test T4'''
+        df = pd.read_csv("many-tools-classifications.csv")
+        a = df[['classification_id', 'user_id', 'subject_ids']]
+        b = df['annotations'].apply(json.loads)
+        
+        expected = [(409.25, 133.96665954589844), (370.25, 123.96665954589844)]
+
+        result = T4(a, b)
+        result = result["coords"].iloc[0]
+
+        self.assertEqual(result, expected)
+
+    def test_last(self):
+        '''Test T4'''
+        df = pd.read_csv("many-tools-classifications.csv")
+        a = df[['classification_id', 'user_id', 'subject_ids']]
+        b = df['annotations'].apply(json.loads)
+        
+        expected = [(300.25, 97.96665954589844) , (406.25, 74.96665954589844)]
+
+        result = T4(a, b)
+        result = result["coords"].iloc[13]
+
+        self.assertEqual(result, expected)
